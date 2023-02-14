9f380dbf45dfffd5b1663a7e67c8dea14568c7b7
DaliaRefaat <125277143+DaliaRefaat@users.noreply.github.com>
2023-02-14 12:19:02 +0200

    Add files via upload

---
+++ source/assets/js/main.js
@@ -0,0 +1,57 @@
+// Date picker jquery ui
+$(function() {
+    $(".date").datepicker();
+});
+
+// Start owel carousel
+$(document).ready(function(){
+    $(".article .owl-carousel").owlCarousel({
+            loop: true,
+            margin: 20,
+            // autoplay: true,
+            rtl: true,
+            responsiveClass: true,
+            responsive: {
+            0: {
+                items: 1,
+                nav: true
+            },
+            600: {
+                items: 2,
+                nav: true
+            },
+            1000: {
+                items: 3,
+                nav: true,
+                loop: true
+            }
+        }
+    });
+});
+
+//Add and remove class active nav-bar by url location
+const currentLocation = location.href;
+const navBarLink = document.querySelectorAll('.nav-link');
+
+navBarLink.forEach(navLink => {
+    if(navLink.href === currentLocation) {
+        navLink.parentElement.classList.add('active');
+    } else {
+        navLink.parentElement.classList.remove('active'); 
+    }
+});
+
+//Show or hide drop down menu profile
+const btnDropDown = document.querySelector('#drop-menu');
+const menuDropDowm = document.querySelector('.profile-menu');
+
+btnDropDown.addEventListener('click', () => {
+    menuDropDowm.classList.toggle('active');
+});
+
+// Start owel carousel
+$(document).ready(function(){
+    $(".article .owl-carousel").owlCarousel({
+        rtl: false,
+    });
+});
